9e45214b5a88a5cfffccf951459d08e543857ca2
Cleber Castiglioni <cleber.castiglioni@gmail.com>
2025-03-04 15:37:55 -0300

    ajustes exercicios ajuste funcao aula2

--- exercicios_ufn/aula2/InsertBuscaArrList.java
+++ exercicios_ufn/aula2/InsertBuscaArrList.java
@@ -8,7 +8,7 @@ import java.util.Vector;
 
 public class InsertBuscaArrList {
     public static void main(String[] args) {
-      List<Violao> ObjvetVl = new Vector<>();
+        List<Violao> ObjvetVl = new Vector<>();
         List<Violao> ObjarrVl = new ArrayList<>();
         List<Violao> ObjlinkVl = new LinkedList<>();
         Scanner ObjScanner = new Scanner(System.in);
@@ -19,48 +19,29 @@ public class InsertBuscaArrList {
         System.out.println("Escolha a estrutura de dados: \n1: Vector \n2: ArrayList \n3: LinkedList");
         int useMetodo = Integer.parseInt(ObjScanner.nextLine());
 
-        long startTime, endTime, elapsedTime;  
-
         if (useMetodo == 1) {
-            startTime = System.nanoTime(); //  cronômetro
-            
-            for (int i = 0; i <= numInsert; i++) {
-                Violao Objv = new Violao(i, "Clássico", 100 + i);
-                ObjvetVl.add(Objv);
-                System.out.println("Insert Vector: " + i);
-            }
-            
-            endTime = System.nanoTime(); 
-            elapsedTime = (endTime - startTime) / 1_000_000; 
-            System.out.println("Tempo de execução para Vector: " + elapsedTime + " ms");
-
+            medirTempoExecucao(ObjvetVl, numInsert, "Vector");
         } else if (useMetodo == 2) {
-            startTime = System.nanoTime(); // Inicia o cronômetro
-            
-            for (int i = 0; i <= numInsert; i++) {
-                Violao Objv = new Violao(i, "Clássico", 100 + i);
-                ObjarrVl.add(Objv);
-                System.out.println("Insert ArrayList: " + i);
-            }
-            
-            endTime = System.nanoTime(); // Finaliza o cronômetro
-            elapsedTime = (endTime - startTime) / 1_000_000; // Convertendo para milissegundos
-            System.out.println("Tempo de execução para ArrayList: " + elapsedTime + " ms");
-
+            medirTempoExecucao(ObjarrVl, numInsert, "ArrayList");
         } else {
-            startTime = System.nanoTime(); // Inicia o cronômetro
-            
-            for (int i = 0; i <= numInsert; i++) {
-                Violao Objv = new Violao(i, "Clássico", 100 + i);
-                ObjlinkVl.add(Objv);
-                System.out.println("Insert LinkedList: " + i);
-            }
-            
-            endTime = System.nanoTime(); // Finaliza o cronômetro
-            elapsedTime = (endTime - startTime) / 1_000_000; // Convertendo para milissegundos
-            System.out.println("Tempo de execução para LinkedList: " + elapsedTime + " ms");
+            medirTempoExecucao(ObjlinkVl, numInsert, "LinkedList");
         }
 
         ObjScanner.close();
+    }
+
+ 
+    public static void medirTempoExecucao(List<Violao> lista_generic, int numInsert, String nomeLista) {
+        long startTime = System.nanoTime(); 
+
+        for (int i = 0; i <= numInsert; i++) {
+            Violao Objv = new Violao(i, "Clássico", 100 + i);
+            lista_generic.add(Objv);
+            System.out.println("Insert " + nomeLista + ": " + i);
+        }
+
+        long endTime = System.nanoTime(); 
+        long elapsedTime = (endTime - startTime) / 1_000_000; 
+        System.out.println("Tempo de execução para " + nomeLista + ": " + elapsedTime + " ms" + "Para:"+ numInsert+ " Elementos");
     }
 }
